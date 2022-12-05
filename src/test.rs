bd1fa72e31393b8061e58a6ffc84b170d53e34bf
Ibby <33922797+SleepySwords@users.noreply.github.com>
2022-12-05 21:28:17 +1100

    feat: fix tests + add more tests

--- src/test.rs
+++ src/test.rs
@@ -154,10 +154,25 @@ mod tags {
     use tui::style::Color;
 
     use crate::{
-        app::TaskStore,
+        app::{App, TaskStore},
+        component::layout::stack_layout::StackLayout,
         task::Task,
         utils::test::{input_char, input_code, setup},
     };
+
+    fn add_tag(app: &mut App, stack_layout: &mut StackLayout, name: &str, colour: &str) {
+        input_char('t', app, stack_layout);
+        input_code(KeyCode::Enter, app, stack_layout);
+
+        name.chars()
+            .for_each(|chr| input_char(chr, app, stack_layout));
+        input_code(KeyCode::Enter, app, stack_layout);
+
+        colour
+            .chars()
+            .for_each(|chr| input_char(chr, app, stack_layout));
+        input_code(KeyCode::Enter, app, stack_layout);
+    }
 
     #[test]
     fn test_tag_creation() {
@@ -172,20 +187,12 @@ mod tags {
             tags: BTreeMap::new(),
         });
 
-        input_char('t', &mut app, &mut stack_layout);
-        input_code(KeyCode::Enter, &mut app, &mut stack_layout);
+        let mut tag_count = 0;
 
-        TEST_TAG
-            .chars()
-            .for_each(|chr| input_char(chr, &mut app, &mut stack_layout));
-        input_code(KeyCode::Enter, &mut app, &mut stack_layout);
+        add_tag(&mut app, &mut stack_layout, TEST_TAG, "#aabbcc");
+        tag_count += 1;
 
-        "aabbcc"
-            .chars()
-            .for_each(|chr| input_char(chr, &mut app, &mut stack_layout));
-        input_code(KeyCode::Enter, &mut app, &mut stack_layout);
-
-        assert_eq!(app.task_store.tasks[0].tags.len(), 1);
+        assert_eq!(app.task_store.tasks[0].tags.len(), tag_count);
         assert_eq!(
             app.task_store.tasks[0].first_tag(&app).unwrap().name,
             TEST_TAG
@@ -194,6 +201,96 @@ mod tags {
             app.task_store.tasks[0].first_tag(&app).unwrap().colour,
             Color::Rgb(170, 187, 204)
         );
+
+        add_tag(&mut app, &mut stack_layout, "Second tag", "Re-D");
+        tag_count += 1;
+
+        assert_eq!(app.task_store.tasks[0].tags.len(), tag_count);
+        assert_eq!(
+            app.task_store
+                .tags
+                .get(app.task_store.tasks[0].tags.last().unwrap())
+                .unwrap()
+                .name,
+            "Second tag"
+        );
+        assert_eq!(
+            app.task_store
+                .tags
+                .get(app.task_store.tasks[0].tags.last().unwrap())
+                .unwrap()
+                .colour,
+            Color::Red
+        );
+
+        add_tag(&mut app, &mut stack_layout, TEST_TAG, "12");
+        tag_count += 1;
+
+        assert_eq!(app.task_store.tasks[0].tags.len(), tag_count);
+        assert_eq!(
+            app.task_store
+                .tags
+                .get(app.task_store.tasks[0].tags.last().unwrap())
+                .unwrap()
+                .name,
+            TEST_TAG
+        );
+        assert_eq!(
+            app.task_store
+                .tags
+                .get(app.task_store.tasks[0].tags.last().unwrap())
+                .unwrap()
+                .colour,
+            Color::Indexed(12)
+        );
+    }
+
+    #[test]
+    fn test_tag_cancel_and_enter() {
+        const TEST_TAG: &str = "WOOO TAGS!!";
+
+        let (mut app, mut stack_layout) = setup(TaskStore {
+            tasks: vec![
+                Task::from_string(String::from("meme")),
+                Task::from_string(String::from("oof")),
+            ],
+            completed_tasks: vec![],
+            tags: BTreeMap::new(),
+        });
+        add_tag(&mut app, &mut stack_layout, TEST_TAG, "ewfnjaweknf");
+        input_code(KeyCode::Enter, &mut app, &mut stack_layout);
+
+        assert_eq!(app.task_store.tags.len(), 0);
+
+        "12".chars()
+            .for_each(|chr| input_char(chr, &mut app, &mut stack_layout));
+        input_code(KeyCode::Enter, &mut app, &mut stack_layout);
+
+        assert_eq!(app.task_store.tags.len(), 1);
+    }
+
+    #[test]
+    fn test_tag_removal() {
+        const TEST_TAG: &str = "WOOO TAGS!!";
+
+        let (mut app, mut stack_layout) = setup(TaskStore {
+            tasks: vec![
+                Task::from_string(String::from("meme")),
+                Task::from_string(String::from("oof")),
+            ],
+            completed_tasks: vec![],
+            tags: BTreeMap::new(),
+        });
+        add_tag(&mut app, &mut stack_layout, TEST_TAG, "1");
+        assert_eq!(app.task_store.tags.len(), 1);
+
+        input_char('t', &mut app, &mut stack_layout);
+        input_code(KeyCode::Down, &mut app, &mut stack_layout);
+        input_code(KeyCode::Enter, &mut app, &mut stack_layout);
+        input_code(KeyCode::Enter, &mut app, &mut stack_layout);
+        input_code(KeyCode::Enter, &mut app, &mut stack_layout);
+
+        assert_eq!(app.task_store.tags.len(), 0);
     }
 }
 
