acc3646d7e899a114d0e25cac3cb384f72187dd2
Froloket64 <ket.frol2006@gmail.com>
2023-07-06 15:03:42 +0300

    Use `add_task` in task addition

--- src/screens/main_screen.rs
+++ src/screens/main_screen.rs
@@ -60,9 +60,10 @@ impl DrawableComponent for MainScreenLayer {
                 let add_input_dialog = InputBoxBuilder::default()
                     .title(String::from("Add a task"))
                     .callback(move |app, word| {
-                        app.task_store
-                            .tasks
-                            .push(Task::from_string(word.trim().to_string()));
+                        app.task_store.add_task(
+                            Task::from_string(word.trim().to_string())
+                        );
+
                         Ok(())
                     })
                     .save_mode(app)
